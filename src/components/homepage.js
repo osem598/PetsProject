4a162bfd5acc0d8df3929f3ec342d09b8c65d1cc
osem598 <osem598@hotmail.com>
2020-08-17 09:31:12 -0600

    styles added

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="homeClass">
+        <div>
+        <img src="https://1.bp.blogspot.com/-WMEzzKn3668/W2hH8doPYKI/AAAAAAAAqvc/ABDiBCwMXGIh-o2F-VNfe5cn3RYxmzc4wCLcBGAs/s1600/Flag_of_Turkmenistan.gif" className="flag"></img>
+        </div>
+        <div>
         Welcome to pets.tm! The best petstore in turkmenistan!!
+        </div>
       </div>
     );
   }
